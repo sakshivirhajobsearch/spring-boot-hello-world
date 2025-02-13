d48c6fa9b72baddf43b84b51f25c0a28da5541c1
Anurag Bharti <anuragvasubharti@gmail.com>
2025-02-13 17:55:23 +0530

    Updated code

--- src/main/java/com/javapoint/HelloController.java
+++ src/main/java/com/javapoint/HelloController.java
@@ -1,13 +1,14 @@
 package com.javapoint;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class MainApplication {
+@RestController
+public class HelloController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
+	@RequestMapping("/")
+	String hello() {
+		return "Hello World, Spring Boot!";
 	}
 
 }
